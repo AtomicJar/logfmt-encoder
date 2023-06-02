5d3d4f2b408e8e11ce5b5ed924918ef5b0897a84
Alex Stockinger <alex@atomicjar.com>
2023-06-02 08:50:51 +0200

    Use assertj
    Signed-off-by: Alex Stockinger <alex@atomicjar.com>

--- src/test/java/com/atomicjar/logging/LogFmtMarkerTest.java
+++ src/test/java/com/atomicjar/logging/LogFmtMarkerTest.java
@@ -2,11 +2,13 @@ package com.atomicjar.logging;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LogFmtMarkerTest {
     @Test
     public void testImmutability() {
         var orig = LogFmtMarker.with("a", "b");
         orig.and("c", "d");
-        assert orig.getData().size() == 1;
+        assertThat(orig.getData()).hasSize(1);
     }
 }
